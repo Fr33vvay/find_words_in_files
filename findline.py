da6359f2fcf2b14fe1031b8f1e8e663a2e673e6a
Илья <7634216@gmail.com>
2020-12-10 16:38:35 +0300

    1. обработка ошибок кодировки 2. исправлен баг с однострочным файлом 3. добавлен вывод количества найденных строк

--- findline.py
+++ findline.py
@@ -14,18 +14,28 @@ def query_list():
 
 def search_word(word, file):
     """Выписывает строки, содержащие искомое слово"""
-    with open(file, encoding='windows-1251') as f, open('strings.txt', 'a') as new_file:
-        for line in f:
-            if word in line:
-                new_file.write(line)
+    count = 0  # Совпавшие строки
+    try:
+        with open(file, encoding='windows-1251') as f, open('strings.txt', 'a') as new_file:
+            for line in f:
+                if word in line:
+                    if line[-1] != '\n':
+                        line += '\n'
+                    count += 1
+                    new_file.write(line)
+    except UnicodeDecodeError:
+        print(f'Ошибка кодировки в {file}')
+    return count
 
 
 def search_files(folder):
     """Ищет слово во всех txt-файлах в указанной папке"""
+    sum_count = 0
     for query in query_list():
         for dirpath, dirnames, filenames in os.walk(folder):
             for filename in [f for f in filenames if f.endswith(".txt")]:
-                search_word(query, os.path.join(dirpath, filename))
+                sum_count += search_word(query, os.path.join(dirpath, filename))
+    print(f'Найдено {sum_count} строк.')
 
 
 if __name__ == '__main__':
